87fd39cbcf9324ea78ec8c0b340bb4f9ae205696
Ben Steinberg <bsteinberg@law.harvard.edu>
2020-03-30 08:44:33 -0400

    Add if-empty option

--- backup.py
+++ backup.py
@@ -18,32 +18,34 @@ CONTEXT_SETTINGS = dict(auto_envvar_prefix='RCON',
               help='Directory to back up')
 @click.option('--directory', default='backups',
               help='Directory for storing backups')
+@click.option('--if-empty/--not-if-empty', default=False,
+              help='Back up only when no players are present')
 @click.option('--source', help='Source, e.g. server name',
               default=lambda: socket.gethostname())
 @click.option('--sync', help='Server to sync to, not yet implemented')
-def cli(password, port, world, directory, source, sync):
+def cli(password, port, world, directory, if_empty, source, sync):
     """
     This program backs up a local Minecraft server instance
     """
     with MCRcon('localhost', password, port=port) as mcr:
-        r = mcr.command('/list')
-        count = int(r.split()[2])
-        if count != 0:
-            are = 'is' if count == 1 else 'are'
-            s = '' if count == 1 else 's'
-            print(f'There {are} {count} player{s} online, stopping')
-        else:
-            print('Saving world...')
-            r = mcr.command('/save-all')
-            print('Turning save off...')
-            r = mcr.command('/save-off')
-            timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-            filename = f'world-{source}-{timestamp}.tar.xz'
-            if not os.path.isdir(directory):
-                os.makedirs(directory)
-            with tarfile.open(os.path.join(directory, filename),
-                              'w:xz') as tar:
-                tar.add(world)
-            print(f'Wrote world to {filename}')
-            print('Turning save back on.')
-            r = mcr.command('/save-on')
+        if if_empty:
+            r = mcr.command('/list')
+            count = int(r.split()[2])
+            if count != 0:
+                are = 'is' if count == 1 else 'are'
+                s = '' if count == 1 else 's'
+                print(f'There {are} {count} player{s} online, stopping')
+                return
+        print('Saving world...')
+        r = mcr.command('/save-all')
+        print('Turning save off...')
+        r = mcr.command('/save-off')
+        timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+        filename = f'world-{source}-{timestamp}.tar.xz'
+        if not os.path.isdir(directory):
+            os.makedirs(directory)
+        with tarfile.open(os.path.join(directory, filename), 'w:xz') as tar:
+            tar.add(world)
+        print(f'Wrote world to {filename}')
+        print('Turning save back on.')
+        r = mcr.command('/save-on')
