44698f4c669b53be3cd5ea94143de4b696249074
Ben Steinberg <bsteinberg@law.harvard.edu>
2020-03-29 22:30:04 -0400

    Remove server option

--- backup.py
+++ backup.py
@@ -10,16 +10,15 @@ CONTEXT_SETTINGS = dict(auto_envvar_prefix='RCON',
 
 
 @click.command(context_settings=CONTEXT_SETTINGS)
-@click.option('--server', default='localhost')
 @click.option('--password', required=True)
 @click.option('--world', default='/home/minecraft/world',
               help='Directory to back up')
-@click.option('--sync', help='Server to sync to')
-def cli(server, password, world, sync):
+@click.option('--sync', help='Server to sync to, not yet implemented')
+def cli(password, world, sync):
     """
     This program backs up a local Minecraft server instance
     """
-    with MCRcon(server, password) as mcr:
+    with MCRcon('localhost', password) as mcr:
         r = mcr.command('/list')
         count = int(r.split()[2])
         if count != 0:
@@ -30,8 +29,7 @@ def cli(server, password, world, sync):
             print('Turning save off...')
             r = mcr.command('/save-off')
             timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-            with tarfile.open(f'world-{server}-{timestamp}.tar.xz',
-                              'w:xz') as tar:
+            with tarfile.open(f'world-{timestamp}.tar.xz', 'w:xz') as tar:
                 tar.add(world)
             print('Turning save back on.')
             r = mcr.command('/save-on')
