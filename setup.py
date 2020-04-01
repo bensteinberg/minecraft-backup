096b27df6f02c420b9d3e0940911c70b601348be
Ben Steinberg <bsteinberg@law.harvard.edu>
2020-04-01 09:25:26 -0400

    Bump version, fill out install_requires

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup
 
 setup(
     name='minecraft-backup',
-    version='0.1',
+    version='0.2',
     py_modules=['backup'],
     install_requires=[
         'Click',
+        'python-dotenv',
+        'mcrcon'
     ],
     entry_points='''
         [console_scripts]
